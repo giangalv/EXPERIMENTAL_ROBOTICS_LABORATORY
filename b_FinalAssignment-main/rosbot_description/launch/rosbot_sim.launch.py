049616a58e42f5d065cae0ad32af88729bf798ca
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2024-01-10 18:26:11 +0100

    Add files via upload

---
+++ b_FinalAssignment-main/rosbot_description/launch/rosbot_sim.launch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+  rosbot_description = get_package_share_directory('rosbot_description')
+  world = LaunchConfiguration('world', default=os.path.join(rosbot_description, 'worlds', 'assignment2.world'))
+
+  return LaunchDescription([
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([rosbot_description, '/launch/world.launch.py']),
+      launch_arguments = {
+        'world' : world,
+      }.items(),
+    ),
+
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([rosbot_description, '/launch/rosbot_spawn.launch.py']),
+    ),
+
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([rosbot_description, '/launch/rviz2.launch.py'])
+    ),
+  ])
+
+
+if __name__ == '__main__':
+  generate_launch_description()
---
+++ b_FinalAssignment-main/rosbot_description/launch/rosbot_spawn.launch.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import os
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+
+def generate_launch_description():
+
+  use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+  use_gpu = LaunchConfiguration('use_gpu', default=False)
+  rosbot_description = get_package_share_directory('rosbot_description')
+  xacro_file = os.path.join(rosbot_description, 'models', 'rosbot', 'rosbot.urdf.xacro')
+
+  # decide the position of the robot
+  pos_x = '1.0'
+  pos_y = '1.0'
+  pos_z = '0.5'
+
+  use_sim = use_sim_time
+  
+
+  robot_description = {
+    'robot_description' : Command([
+      'xacro --verbosity 0 ', xacro_file,
+      ' use_sim:=', use_sim,
+      ' use_gpu:=', use_gpu
+    ])
+  }
+
+
+  return LaunchDescription([
+
+    Node(
+      package='robot_state_publisher',
+      executable='robot_state_publisher',
+      output='screen',
+      parameters=[
+        {'use_sim_time': use_sim_time},
+        robot_description
+      ]
+    ),
+    
+    Node(
+      package='gazebo_ros',
+      executable='spawn_entity.py',
+      # output='screen',
+      parameters=[{'use_sim_time': use_sim_time}],
+      arguments=['-timeout', '600',
+                 '-entity', 'rosbot',
+                 '-x', pos_x, '-y', pos_y, '-z', pos_z,
+                 '-topic', 'robot_description']
+    ),
+  ])
+
+if __name__ == '__main__':
+  generate_launch_description()
